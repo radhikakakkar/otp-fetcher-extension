f20f3ed903727a5480d2b5ce91c581d58ecb87c7
Radhika Kakkar <radhika.199302152@muj.manipal.edu>
2024-02-28 13:02:47 +0530

    improved styling

--- scripts.js
+++ scripts.js
@@ -4,6 +4,12 @@ let client_json = {};
 console.log("I'm script.js");
 //on click function calls
 document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("client_creds") != null) {
+    document.getElementById("connect-account-p").textContent =
+      "Your account is connected";
+    document.getElementById("connect-account-btn").textContent = "Proceed";
+  }
+
   document
     .getElementById("connect-account-btn")
     .addEventListener("click", function () {
@@ -23,12 +29,58 @@ async function connectAccount() {
   console.log("Connect account function called");
 
   if (localStorage.getItem("client_creds") != null) {
+    // document.getElementById("connect-account-p").textContent =
+    //   "Your account is connected";
+    // document.getElementById("connect-account-btn").textContent = "Proceed";
+
     console.log("getting client from local");
     client = JSON.parse(localStorage.getItem("client_creds"));
     console.log(client);
   } else {
     console.log("generating new client and saving in local storage");
-    client = await fetch("http://localhost:3000/authorize");
+
+    //opening a new window for google auth STARTS
+
+    // const newWindow = window.open(
+    //   "http://localhost:3000/authorize",
+    //   "_blank",
+    //   "width=500,height=500"
+    // );
+    // newWindow.focus();
+    // function receiveMessage(event) {
+    //   if (event.origin === "http://localhost:3000") {
+    //     console.log("Received data from child window:", event.data);
+    //   }
+    // }
+    // newWindow.addEventListener("message", receiveMessage, false);
+    // const result = receiveMessage();
+    // newWindow.opener.postMessage(result, "http://localhost:3000");
+
+    // Define the function to handle messages from the child window
+    let dataToSend;
+    function receiveMessage(event) {
+      if (event.origin === "nognbilglhmklbccnilbollghnkfgbbj") {
+        console.log("Received data from child window:", event.data);
+      }
+    }
+    window.addEventListener("message", receiveMessage, false);
+    const newWindow = window.open(
+      "http://localhost:3000/redirect-to-auth",
+      "",
+      "width=100px,height=100px"
+    );
+
+    // newWindow.addEventListener("load", async function () {
+    dataToSend = await fetch("http://localhost:3000/authorize");
+    // console.log(dataToSend);
+    // newWindow.postMessage(dataToSend, "nognbilglhmklbccnilbollghnkfgbbj");
+    // });
+
+    //opening a new window for google auth ENDS
+
+    // client = await fetch("http://localhost:3000/authorize");
+    client = dataToSend;
+    console.log(dataToSend);
     client_json = await client.json();
     localStorage.setItem("client_creds", JSON.stringify(client_json));
   }
@@ -70,7 +122,6 @@ async function loadResults() {
       const date_text = document.createTextNode(
         "Date: " + " " + JSON.stringify(data[i]["date"])
       );
-      console.log(date_text);
 
       //display otp
       const node_otp = document.createElement("p");
@@ -79,6 +130,7 @@ async function loadResults() {
 
       //display break
       const node_br = document.createElement("br");
+      const node_hr = document.createElement("hr");
 
       node_sender_email.appendChild(sender_email_text);
       node_date.appendChild(date_text);
@@ -87,7 +139,7 @@ async function loadResults() {
       container.appendChild(node_date);
       container.appendChild(node_br);
       container.appendChild(node_otp);
-      container.appendChild(node_br);
+      container.appendChild(node_hr);
     }
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -96,6 +148,11 @@ async function loadResults() {
 
 function logout() {
   localStorage.clear();
+  if (localStorage.getItem("client_creds") == null) {
+    document.getElementById("connect-account-p").textContent =
+      "Connect Your Google Account to use the feature!";
+    document.getElementById("connect-account-btn").textContent = "Connect";
+  }
   console.log("You have been logged out");
   connect_account_container = document.getElementById(
     "connect-google-account-container"
